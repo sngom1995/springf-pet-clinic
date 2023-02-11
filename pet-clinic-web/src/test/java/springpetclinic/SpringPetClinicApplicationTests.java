d11400ab8f045a4ad43c209dcfda5a5b587543f9
sngom1995 <ngomsamba75@gmail.com>
2023-02-11 20:17:22 +0000

    implement pet clinic maven multi module maven add.

--- pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -2,8 +2,9 @@ package springpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import sn.springframework.sfgpetclinic.SpringPetClinicApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringPetClinicApplication.class)
 class SpringPetClinicApplicationTests {
 
 	@Test
