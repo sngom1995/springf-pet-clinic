21f8a3939bc7ae366d233d1488e70c9dd1aa3038
sngom1995 <ngomsamba75@gmail.com>
2023-02-12 13:27:34 +0000

    Refactor Service Interface to Common Base Interface. Closes #11

--- pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/services/PetService.java
@@ -6,8 +6,6 @@ import sn.springframework.sfgpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService  extends CrudService<Pet, Long>{
+
 }
