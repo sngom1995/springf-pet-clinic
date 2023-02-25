0408de252cead09eaab00db788935c5a18bab3ec
sngom1995 <ngomsamba75@gmail.com>
2023-02-25 20:16:21 +0000

    convert Visit to JPA entity.

--- pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/model/Pet.java
@@ -3,6 +3,8 @@ package sn.springframework.sfgpetclinic.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -15,6 +17,9 @@ public class Pet extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
--- pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/model/Visit.java
@@ -1,12 +1,18 @@
 package sn.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
-
-    private LocalDate date;
-
+        @Column(name = "date")
+        private LocalDate date;
+        @Column(name = "description")
         private String description;
+        @ManyToOne
+        @JoinColumn(name = "pet_id")
         private Pet pet;
 
         public String getDescription() {
