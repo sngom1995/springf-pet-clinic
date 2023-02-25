b7f6259c3c3f8300cb1c846c5995fc8015886041
sngom1995 <ngomsamba75@gmail.com>
2023-02-25 19:59:39 +0000

    convert owner to JPA entity. Closes #35

--- pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/model/Owner.java
@@ -1,17 +1,20 @@
 package sn.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
-
-
-
-
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
-
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/sn/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,11 @@
 package sn.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
 public class PetType extends BaseEntity {
+    @Column(name = "name")
     private String name;
 
     public String getName() {
