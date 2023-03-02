0ae7cce6869719bab0007d56fa5e1707c9ed46ae
sngom1995 <ngomsamba75@gmail.com>
2023-03-02 22:42:02 +0000

    update to JUnit5. Closes #4

--- pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,10 +1,13 @@
 package springpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import sn.springframework.sfgpetclinic.SpringPetClinicApplication;
 
-@SpringBootTest(classes = SpringPetClinicApplication.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class SpringPetClinicApplicationTests {
 
 	@Test
